656760d5853f2c348728d195fc3586741edfcf55
Debarpan Das <48771862+Debarpan98@users.noreply.github.com>
2021-01-05 20:46:05 -0700

    Upload

---
+++ Unet/run_training.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --account def-punithak
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=6
+#SBATCH --mem=20G
+#SBATCH --time=0-8:00
+#SBATCH --mail-user=debarpan1998@gmail.com
+#SBATCH --mail-type=ALL
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+
+module load python/3.7 cuda cudnn
+source /home/debarpan/scratch/retina-unet/RV/bin/activate RV
+python run_training.py
